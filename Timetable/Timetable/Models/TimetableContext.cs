a84fe18b97cafc677eda11085600b0c61a7572d4
JennieBolotnikova <bolotnikova.jenni@gmail.com>
2021-10-10 23:13:36 +0300

    Заполнение базы данных тестовым набором записей

--- Timetable/Timetable/Models/TimetableContext.cs
+++ Timetable/Timetable/Models/TimetableContext.cs
@@ -29,5 +29,41 @@ namespace Timetable.Models
         {
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-B9763I0\SQL_EXPRESS;Database=Timetable;Trusted_Connection=True;");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ActivityType>().HasData(
+                new ActivityType[]
+                {
+                new ActivityType { ActivityTypeID  = 1, ActivityTypeName = "Лекция", ActivityTypesShortName = "ЛК"},
+                new ActivityType { ActivityTypeID  = 2, ActivityTypeName = "Лабораторное занятие", ActivityTypesShortName = "ЛЗ"},
+                new ActivityType { ActivityTypeID  = 3, ActivityTypeName = "Практическое заняти", ActivityTypesShortName = "ПЗ"}
+                });
+
+            modelBuilder.Entity<Building>().HasData(
+                new Building[]
+                {
+                new Building { BuildingID  = 1, BuildingName = "Первый корпус"},
+                new Building { BuildingID  = 2, BuildingName = "Второй корпус"},
+                });
+
+            modelBuilder.Entity<ClassroomType>().HasData(
+                new ClassroomType[]
+                {
+                new ClassroomType { ClassroomTypeID  = 1, СlassroomTypeName = "Лекционная аудитория"},
+                new ClassroomType { ClassroomTypeID  = 2, СlassroomTypeName = "Аудиотрия для практических и лабораторных занятий"},
+                });
+
+            modelBuilder.Entity<Day>().HasData(
+                new Day[]
+                {
+                new Day { DayID  = 1, DayName = "Понедельник", DayShortName = "ПН"},
+                new Day { DayID  = 2, DayName = "Вторник", DayShortName = "ВТ"},
+                new Day { DayID  = 3, DayName = "Среда", DayShortName = "СР"},
+                new Day { DayID  = 4, DayName = "Четверг", DayShortName = "ЧТ"},
+                new Day { DayID  = 5, DayName = "Пятница", DayShortName = "ПТ"},
+                new Day { DayID  = 6, DayName = "Суббота", DayShortName = "СБ"}
+                });
+        }
     }
 }
